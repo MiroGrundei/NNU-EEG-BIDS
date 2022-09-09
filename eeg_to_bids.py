e926005630355aea249cdd025f2dc0976774ae27
MiroGrundei <m.grundei@posteo.de>
2022-09-09 11:39:47 +0200

    Add files via upload
    Optimised json encoding, added notebook as example

--- eeg_to_bids.py
+++ eeg_to_bids.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-
+import json
 
 class EEG_BIDS:
 
@@ -12,123 +12,122 @@ class EEG_BIDS:
         if not os.path.exists(self.out_dir):
             os.makedirs(self.out_dir)
 
-        self.startjson, self.endjson = ['{\n'], ['\n}']
-
     def dataset_description(self, dataset_name, data_type, authors, acknowledgement, dataset_doi):
 
-        dataset_name = ['\t"Name": "{}",\n'.format(dataset_name)]
-        author_list = ['\t"Authors": [\n']
-        for author in authors:
-            if not author == authors[-1]:
-                author_list.append('\t\t\t\t\t\t"{}",\n'.format(author))
-            else:
-                author_list.append('\t\t\t\t\t\t"{}"'.format(author))
-        author_list.append('\n\t\t\t\t\t\t],\n')
-        data_type = ['\t"DatasetType": "{}",\n'.format(data_type)]
-        bids_version = ['\t"BIDSVersion": "1.4.0",\n']
-        acknowledgement = ['\t"HowToAcknowledge": "{}",\n'.format(acknowledgement)]
-        doi = ['\t"DatasetDOI": "{}"'.format(dataset_doi)]
+        json_dict = {
+            "Name": dataset_name,
+            "BIDSVersion": "1.4.0",
+            "DatasetType": data_type,
+            "Authors": authors,
+            "HowToAcknowledge": acknowledgement,
+            "DatasetDOI": dataset_doi
+        }
 
-        write_lines = self.startjson + dataset_name + bids_version + data_type + author_list + acknowledgement + doi + self.endjson
-        with open('{}/dataset_description.json'.format(self.out_dir), 'w') as f:
-            for line in write_lines:
-                f.write(''.join(line))
+        json_object = json.dumps(json_dict, indent=4)
+        with open('{}/dataset_description.json'.format(self.out_dir), 'w') as outfile:
+            outfile.write(json_object)
 
     def init_sj_dir(self, subject_number):
+
         self.subject = 'sub-{:02d}'.format(subject_number)
         self.sj_dir = '{}/{}/eeg'.format(self.out_dir, self.subject)
         if not os.path.exists(self.sj_dir):
             os.makedirs(self.sj_dir)
 
     def copy_eeg_file(self, eeg_file):
+
         out_file = os.path.join(self.sj_dir, '{}_task-{}_eeg.bdf'.format(self.subject, self.proj_name))
         shutil.copyfile(eeg_file, out_file)
 
     def electrode_localisation(self, sfp_file, write_tsv_file=True):
+
         if not os.path.exists(sfp_file):
             raise ValueError('Cannot find SFP localizer file!')
 
         fiducials, channels = [], []
-        write_lines = ['name\t\t\tx\t\ty\t\tz']
-        with open(sfp_file, 'r') as f:
-            lines = f.readlines()
+        write_lines = ['name\tx\ty\tz']
+        with open(sfp_file, 'r') as infile:
+            lines = infile.readlines()
             for line in lines:
                 if line.startswith('fid'):
-                    fiducials.append(line)
+                    fiducials.append(line.split()[1:])
                 if not line.startswith('fid'):
-                    write_lines.append(line)
-                    i = line.find('\t')
-                    channels.append(line[0:i])
-            write_lines = [line.rstrip() for line in write_lines]
-            write_lines = [line for line in write_lines if not line == '']
+                    line_elements = line.split()
+                    channels.append(line_elements[0])
+                    write_lines.append('{}\t{}\t{}\t{}'.format(line_elements[0], line_elements[1], line_elements[2], line_elements[3]))
 
         self.channels = channels
-
-        fiducials = [fid.rstrip() for fid in fiducials]
-        fiducials = [fiducials[0].replace('fidt9\t\t\t', ''),
-                     fiducials[1].replace('fidnz\t\t\t', ''),
-                     fiducials[2].replace('fidt10\t\t\t', '')]
         self.fiducials = fiducials
 
         if write_tsv_file:
-            with open(os.path.join(self.sj_dir, '{}_task-{}_electrodes.tsv'.format(self.subject, self.proj_name)), 'w') as f:
-                f.write('\n'.join(write_lines))
+            with open(os.path.join(self.sj_dir, '{}_task-{}_electrodes.tsv'.format(self.subject, self.proj_name)), 'w') as outfile:
+                outfile.write('\n'.join(write_lines))
 
     def eeg_description(self, task_name, task_description, instructions):
-        specs = ['\t"TaskName": "{}",\n'.format(task_name), '\t"TaskDescription": "{}",\n'.format(task_description),
-                      '\t"Instructions: ""{}",\n'.format(instructions)]
-        defaults = ['\t"InstitutionName": "Center for Cognitive Neuroscience Berlin (CCNB)",\n',
-                          '\t"SamplingFrequency": {:d},\n'.format(2048),
-                          '\t"Manufacturer": "Brain Products",\n',
-                          '\t"ManufacturersModelName": "BrainAmp DC",\n',
-                          '\t"CapManufacturer": "EasyCap",\n'
-                          '\t"CapManufacturersModelName": "M1-ext",\n',
-                          '\t"EEGChannelCount": {:d},\n'.format(64),
-                          '\t"EOGChannelCount": {:d},\n'.format(4),
-                          '\t"ECGChannelCount": {:d},\n'.format(0),
-                          '\t"EMGChannelCount": {:d},\n'.format(0),
-                          '\t"MiscChannelCount": {:d},\n'.format(0),
-                          '\t"TriggerChannelCount": {:d},\n'.format(1),
-                          '\t"PowerLineFrequency": {:d},\n'.format(50),
-                          '\t"EEGPlacementScheme": "10-20 system",\n',
-                          '\t"EEGReference": "CMS and DRL",\n',
-                          '\t"EEGGround": "CMS and DRL",\n',
-                          '\t"RecordingType": "continuous"']
-        write_lines = self.startjson + specs + defaults + self.endjson
-        with open('{}/{}_task-{}_eeg.json'.format(self.sj_dir, self.subject, self.proj_name), 'w') as f:
-            for line in write_lines:
-                f.write(''.join(line))
+
+        json_dict = {
+            "TaskName": task_name,
+            "TaskDescription": task_description,
+            "Instructions": instructions,
+            "InstitutionName": "Center for Cognitive Neuroscience Berlin (CCNB)",
+            "SamplingFrequency": 2048,
+            "Manufacturer": "Brain Products",
+            "ManufacturersModelName": "BrainAmp DC",
+            "CapManufacturer": "EasyCap",
+            "CapManufacturersModelName": "M1-ext",
+            "EEGChannelCount": 64,
+            "EOGChannelCount": 4,
+            "ECGChannelCount": 0,
+            "EMGChannelCount": 0,
+            "MiscChannelCount": 0,
+            "TriggerChannelCount": 1,
+            "PowerLineFrequency": 50,
+            "EEGPlacementScheme": "10-20 system",
+            "EEGReference": "CMS and DRL",
+            "EEGGround": "CMS and DRL",
+            "RecordingType": "continuous"
+        }
+
+        json_object = json.dumps(json_dict, indent=4)
+        with open('{}/{}_task-{}_eeg.json'.format(self.sj_dir, self.subject, self.proj_name), 'w') as outfile:
+            outfile.write(json_object)
 
     def coordinate_system(self):
-        specs = ['\t"FiducialsCoordinates": {\n',
-                 '\t\t\t\t\t\t\t\t\t\t\t\t\t\t"LPA": [{}],\n'.format(self.fiducials[0]),
-                 '\t\t\t\t\t\t\t\t\t\t\t\t\t\t"NAS": [{}],\n'.format(self.fiducials[1]),
-                 '\t\t\t\t\t\t\t\t\t\t\t\t\t\t"RPA": [{}],\n'.format(self.fiducials[2]),
-                 '\t\t\t\t\t\t\t\t\t\t\t\t\t}']
-        defaults = ['\t"EEGCoordinateSystemDescription": "LPA, NASION, RPA",\n',
-                    '\t"FiducialsDescription": "Electrodes and fiducials were digitised with ZEBRIS (zebris medical gmbh) '
-                    '\tvia electrodes sticked on the left/right pre-auricular points and the nasion",\n']
-        write_lines = self.startjson + defaults + specs + self.endjson
-        with open('{}/{}_task-{}_coordsystem.json'.format(self.sj_dir, self.subject, self.proj_name), 'w') as f:
-            for line in write_lines:
-                f.write(''.join(line))
+
+        json_dict = {
+            "FiducialsCoordinates": {
+                "LPA": [float(self.fiducials[0][0]), float(self.fiducials[0][1]), float(self.fiducials[0][2])],
+                "NAS": [float(self.fiducials[1][0]), float(self.fiducials[1][1]), float(self.fiducials[1][2])],
+                "RPA": [float(self.fiducials[2][0]), float(self.fiducials[2][1]), float(self.fiducials[2][2])],
+            },
+            "EEGCoordinateSystemDescription": "LPA, NASION, RPA",
+            "FiducialsDescription": "Electrodes and fiducials were digitised with ZEBRIS (zebris medical gmbh) "
+                                    "via electrodes sticked on the left/right pre-auricular points and the nasion"
+        }
+
+        json_object = json.dumps(json_dict, indent=4)
+        with open('{}/{}_task-{}_coordsystem.json'.format(self.sj_dir, self.subject, self.proj_name), 'w') as outfile:
+            outfile.write(json_object)
 
     def channel_description(self, trigger_labels):
-        specs = ['Status\tTRIG\t"Trigger labels: {}"'.format(trigger_labels)]
 
-        defaults = ['name\ttype\tdescription\n']
+        write_lines_input = ['Status\tTRIG\t"Trigger labels: {}"'.format(trigger_labels)]
+
+        write_lines_defaults = ['name\ttype\tdescription\n']
+
         for channel in self.channels:
-            defaults.append('{}\tEEG\n'.format(channel))
-        defaults.append(['EXG1\tEOG\n'
-                         'EXG2\tEOG\n'
-                         'EXG3\tEOG\n'
-                         'EXG4\tEOG\n'
-                         'EXG5\tEOG\n'
-                         'EXG6\tEOG\n'
-                         'EXG7\tEOG\n'
-                         'EXG8\tEOG\n'])
+            write_lines_defaults.append('{}\tEEG\n'.format(channel))
 
-        write_lines = defaults + specs
+        write_lines_defaults.append(['EXG1\tEOG\n'
+                                     'EXG2\tEOG\n'
+                                     'EXG3\tEOG\n'
+                                     'EXG4\tEOG\n'
+                                     'EXG5\tEOG\n'
+                                     'EXG6\tEOG\n'
+                                     'EXG7\tEOG\n'
+                                     'EXG8\tEOG\n'])
+
+        write_lines = write_lines_defaults + write_lines_input
         with open('{}/{}_task-{}_channels.tsv'.format(self.sj_dir, self.subject, self.proj_name), 'w') as f:
             for line in write_lines:
                 f.write(''.join(line))
--- transform_exp_data.py
+++ transform_exp_data.py
@@ -14,16 +14,16 @@ dataset_name = 'Tri-modal stimulation with probabilistic roving stimulus sequenc
 data_type = 'raw'
 authors = ['Miro Grundei', 'Felix Blankenburg']
 acknowledgement = 'Please cite this data set by referring to the publication'
-dataset_doi = 'DOI'
+dataset_doi = '1337'
 bids.dataset_description(dataset_name=dataset_name,
                              data_type=data_type,
                              authors=authors,
                              acknowledgement=acknowledgement,
                              dataset_doi=dataset_doi)
 
-n_subs = 34
+n_subs = 1
 rm_subs = [20, 21]
-for sub in [s for s in range(7, n_subs+1) if s not in rm_subs]:
+for sub in [s for s in range(1, n_subs+1) if s not in rm_subs]:
 
     sub_name = 'sub-{:02d}'.format(sub)
     print(sub_name)
@@ -36,7 +36,7 @@ for sub in [s for s in range(7, n_subs+1) if s not in rm_subs]:
     bids.copy_eeg_file(eeg_file=eeg_file)
 
     # create channel localization file ('sub-01_task-crossbl_electrodes.tsv')
-    sfp_file = 'E:\crossBL\data\{}\loc\{}_crossBL.sfp'.format(sub_name, sub_name)
+    sfp_file = 'E:/crossBL/data/{}/loc/{}_crossBL.sfp'.format(sub_name, sub_name)
     bids.electrode_localisation(sfp_file=sfp_file,
                                 write_tsv_file=True)
 
@@ -45,17 +45,21 @@ for sub in [s for s in range(7, n_subs+1) if s not in rm_subs]:
 
     # create eeg system description ('sub-01_task-crossbl_eeg.json')
     task_name = 'Retrospective perceptual decision'
-    task_description = 'Participants are sequentially presented with simultaneous stimuli in three modalities (Tactile=T, Auditory=A, Visual=V) which can each take one of two possible intensity levels (low/high). ' \
-                       'The task is to fixate at the center of the screen with occasional promts to indicate the identity of one of the three modalities (T?/A?/V?) (randomly) of the most recent stimulus. ' \
-                       'Participants can reply via a left or a right footpedal for left=low/right/high or left=high/right=low respectively (pressed with right foot only). '
-    instructions = 'Please fixate at the center of the screen on the fixation cross and attend the stimulation.' \
-                   'Please respond with the foot pedals if the most recent stimulus in the prompted modality (T/A/V) was low or high.'
+    task_description = 'Participants are sequentially presented with simultaneous stimuli in three modalities ' \
+                       '(Tactile=T, Auditory=A, Visual=V) which can each take one of two possible intensity levels ' \
+                       '(low/high). The task is to fixate at the center of the screen with occasional promts to ' \
+                       'indicate the identity of one of the three modalities (T?/A?/V?) (randomly) of the most recent ' \
+                       'stimulus. Participants can reply via a left or a right footpedal for left=low/right/high or ' \
+                       'left=high/right=low respectively (pressed with right foot only).'
+    instructions = 'Please fixate at the center of the screen on the fixation cross and attend the stimulation. ' \
+                   'Please respond with the foot pedals if the most recent stimulus in the prompted modality (T/A/V) ' \
+                   'was low or high.'
     bids.eeg_description(task_name=task_name,
                          task_description=task_description,
                          instructions=instructions)
 
     # create channel description file ('sub-01_task-crossbl_channels.tsv')
     trigger_labels = 'trigger with label 128 indicates a tri-modal stimulation. ' \
-                     'Please refer to the log file for the exact stimulus identity.' \
+                     'Please refer to the log file for the exact stimulus identity. ' \
                      '126 and 127 indicate starting and stopping of EEG system'
     bids.channel_description(trigger_labels)
